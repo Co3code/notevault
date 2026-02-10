047fe0b6313848ad484fd4b378c2fc003ae7f6b3
Co3code <countian3@gmail.com>
2026-02-10 23:07:39 +0800

    Implement soft delete for notes

--- delete_note.php
+++ delete_note.php
@@ -2,7 +2,11 @@
 include "db.php";
 
 $id = $_GET['id'];
-mysqli_query($conn, "DELETE FROM notes WHERE id=$id");
+// mysqli_query($conn, "DELETE FROM notes WHERE id=$id");
+
+//soft delete
+mysqli_query($conn, "UPDATE notes SET deleted_at = NOW() WHERE id = $id");
 
 header("Location: index.php");
 exit;
+// /Implement soft delete for notes
