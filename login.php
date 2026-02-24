87a4ce50fd1fd34ee71dde3a941e77c5998182bf
Co3code <countian3@gmail.com>
2026-02-24 10:21:52 +0800

    inlcude header

--- login.php
+++ login.php
@@ -1,6 +1,8 @@
 <?php
     session_start();
     include "db.php";
+    $pageTitle = "Login Page";
+    include 'header.php';
 
     if (isset($_POST['login'])) {
     $username = $_POST['username'];
@@ -34,7 +36,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login - My Notes App</title>
+    <!-- <title>Login - My Notes App</title> -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
     <style>
