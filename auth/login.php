3d80391ea2273d982fdf3cc29a3972222a35203b
Co3code <countian3@gmail.com>
2026-02-25 00:56:13 +0800

    store role session

--- auth/login.php
+++ auth/login.php
@@ -20,7 +20,12 @@
         if (password_verify($password, $user['password'])) {
             $_SESSION['user_id']  = $user['id'];
             $_SESSION['username'] = $user['username'];
-            header("Location:../index.php");
+            $_SESSION['role']     = $user['role']; // store role session
+            if ($user['role'] == 'admin') {
+                header("location: ../admin/admin_dashboard.php");
+            } else {
+                header("Location:../index.php");
+            }
             exit();
         } else {
             $error = "Wrong password!";
